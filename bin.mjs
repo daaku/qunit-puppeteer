0beeb1e42ccfdc709980c9bb49a2cbe5e883cd49
Naitik Shah <n@daaku.org>
2023-11-28 13:50:04 +0400

    drop the semicolons

--- bin.mjs
+++ bin.mjs
@@ -1,32 +1,32 @@
 #!/usr/bin/env node
-import which from 'which';
-import { mkdirp } from 'mkdirp';
-import fs from 'node:fs/promises';
-import path from 'node:path';
-import puppeteer from 'puppeteer-core';
+import which from 'which'
+import { mkdirp } from 'mkdirp'
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import puppeteer from 'puppeteer-core'
 
-const onConsole = (msg) => {
-  const type = msg.type();
-  const prefix = type === 'log' ? '' : `[${type}] `;
-  console.log(prefix + msg.text());
-};
+const onConsole = msg => {
+  const type = msg.type()
+  const prefix = type === 'log' ? '' : `[${type}] `
+  console.log(prefix + msg.text())
+}
 
 const terminate = (browser, status = 0) => {
-  browser.close();
-  process.exit(status);
-};
+  browser.close()
+  process.exit(status)
+}
 
 const firstPath = async (choices, msg) => {
   for (const choice of choices) {
     try {
-      await fs.stat(choice);
-      return choice;
+      await fs.stat(choice)
+      return choice
     } catch {
-      continue;
+      continue
     }
   }
-  throw new Error(msg);
-};
+  throw new Error(msg)
+}
 
 const browserPath = async () => {
   return await firstPath(
@@ -40,19 +40,19 @@ const browserPath = async () => {
       await which('brave', { nothrow: true }),
     ],
     'no chrome or chromium binary found',
-  );
-};
+  )
+}
 
 const getTimeout = () => {
   if (process.env.TIMEOUT) {
-    return parseInt(process.env.TIMEOUT, 10);
+    return parseInt(process.env.TIMEOUT, 10)
   }
-  return 5000;
-};
+  return 5000
+}
 
 const getURI = async () => {
   if (process.env.URI) {
-    return process.env.URI;
+    return process.env.URI
   }
   const file = await firstPath(
     [
@@ -62,66 +62,66 @@ const getURI = async () => {
       path.resolve('tests/index.html'),
     ],
     'could not determine HTML entrypoint',
-  );
-  return `file://${file}`;
-};
+  )
+  return `file://${file}`
+}
 
 const main = async () => {
-  const start = Date.now();
-  const timeout = getTimeout();
-  const uri = await getURI();
+  const start = Date.now()
+  const timeout = getTimeout()
+  const uri = await getURI()
 
-  const binary = await browserPath();
+  const binary = await browserPath()
   const browser = await puppeteer.launch({
     headless: 'new',
     args: ['--allow-file-access-from-files'],
     executablePath: binary,
-  });
+  })
 
-  const page = await browser.newPage();
-  page.setDefaultNavigationTimeout(timeout);
-  page.on('console', onConsole);
+  const page = await browser.newPage()
+  page.setDefaultNavigationTimeout(timeout)
+  page.on('console', onConsole)
 
   const timer = setTimeout(() => {
-    console.error('Timeout - aborting tests.');
-    terminate(browser, 1);
-  }, timeout);
+    console.error('Timeout - aborting tests.')
+    terminate(browser, 1)
+  }, timeout)
 
-  let resolveRunEnd;
-  const runEnd = new Promise((resolve) => {
-    resolveRunEnd = resolve;
-  });
-  await page.exposeFunction('HARNESS_RUN_END', (data) => resolveRunEnd(data));
+  let resolveRunEnd
+  const runEnd = new Promise(resolve => {
+    resolveRunEnd = resolve
+  })
+  await page.exposeFunction('HARNESS_RUN_END', data => resolveRunEnd(data))
   await page.coverage.startJSCoverage({
     resetOnNavigation: false,
     includeRawScriptCoverage: true,
-  });
-  await page.goto(uri);
+  })
+  await page.goto(uri)
 
-  const { testCounts, runtime } = await runEnd;
-  clearTimeout(timer);
-  const coverage = await page.coverage.stopJSCoverage();
-  const outdir = process.env.NODE_V8_COVERAGE || 'coverage/tmp';
-  await mkdirp(outdir);
+  const { testCounts, runtime } = await runEnd
+  clearTimeout(timer)
+  const coverage = await page.coverage.stopJSCoverage()
+  const outdir = process.env.NODE_V8_COVERAGE || 'coverage/tmp'
+  await mkdirp(outdir)
   await fs.writeFile(
     `${outdir}/out.json`,
     JSON.stringify({
-      result: coverage.map((ci) => ci.rawScriptCoverage),
+      result: coverage.map(ci => ci.rawScriptCoverage),
     }),
-  );
+  )
 
-  const success = testCounts.failed === 0;
+  const success = testCounts.failed === 0
   const prefix = success
     ? `✓ passed ${testCounts.passed}`
-    : `✗ failed ${testCounts.failed}`;
-  const duration = Date.now() - start;
+    : `✗ failed ${testCounts.failed}`
+  const duration = Date.now() - start
   console.error(
     `${prefix} / ${testCounts.total} (tests: ${Math.floor(
       runtime,
     )}ms / total: ${duration}ms)`,
-  );
+  )
 
-  terminate(browser, success ? 0 : 1);
-};
+  terminate(browser, success ? 0 : 1)
+}
 
-await main();
+await main()
